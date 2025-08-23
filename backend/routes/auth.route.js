a506e4a9a54f41ea19de791f96665482a5d523b2
Jose Cruz <159391963+Cruz3196@users.noreply.github.com>
2025-08-23 03:16:23 -0700

    update on the product controller

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -5,5 +5,13 @@ const router = express.Router();
 router.get("/signup", (req,res) => {
     res.json({ message: "endpoint for signup!!!" });
 })
+router.get("/login", (req,res) => {
+    res.json({ message: "endpoint for login!!!" });
+})
+
+router.get("/logout", (req,res) => {
+    res.json({ message: "endpoint for logout!!!" });
+})
+
 
 export default router;
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -5,7 +5,7 @@ from '../controllers/product.controller.js';
 const router = express.Router();
 
 //getting all products, this will eventually be done only by admin
-router.get('/', getAllProducts);
+router.get('/all', getAllProducts);
 //get product by id
 router.get('/:id',getProductId)
 //endpoint for creating a product, this will only be for admin
