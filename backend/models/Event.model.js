4c94a9c48d023853166188dde45ba8c8b51c53ba
Jose Cruz <159391963+Cruz3196@users.noreply.github.com>
2025-08-31 11:00:08 -0700

    event model created

---
+++ backend/models/Event.model.js
@@ -0,0 +1,26 @@
+import e from "express";
+import mongoose from "mongoose";
+
+const eventScheme = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    img:{
+        type: String,
+    },
+    category:{
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const Event = mongoose.model("Event", eventScheme);
+
+export default Product;
