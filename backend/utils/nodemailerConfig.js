6c5f1b362c0879f1cff675685a7345c295ad8b8c
Jose Cruz <159391963+Cruz3196@users.noreply.github.com>
2025-10-04 11:12:36 -0700

    update

--- backend/utils/nodemailerConfig.js
+++ backend/utils/nodemailerConfig.js
@@ -2,8 +2,11 @@ import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // use SSL
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
 });
+
