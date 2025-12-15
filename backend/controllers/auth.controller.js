d1e925910c271254dc6b833b99c95b458f36cd0b
Jose Cruz <159391963+Cruz3196@users.noreply.github.com>
2025-12-15 13:33:08 -0800

    refactored auth controller and updated ui on trainerprofilesetup page

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -5,55 +5,14 @@ import Trainer from "../models/trainer.model.js";
 // importing utilities
 import { redis } from "../lib/redis.js";
 import jwt from "jsonwebtoken";
+import {
+  generateTokens,
+  storageRefreshToken,
+  setCookies,
+} from "../utils/tokenUtils.js";
 
 // import nodemailer transporter
 import { sendWelcomeEmail } from "../utils/emailService.js";
-
-// 15 minutes for storage to host the token
-const generateTokens = (userId) => {
-  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "15m",
-  });
-  // 7 days for storage to host the token
-  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: "7d",
-  });
-  //returning the tokens
-  return { accessToken, refreshToken };
-};
-
-// 7 days for storage to host the token, function for storing the refresh token
-const storageRefreshToken = async (userId, refreshToken) => {
-  await redis.set(
-    `refreshToken:${userId}`,
-    refreshToken,
-    "EX",
-    7 * 24 * 60 * 60
-  );
-};
-//* "accessToken" is the key name, and accessToken is the value, function for setting the cookies
-const setCookies = (res, accessToken, refreshToken) => {
-  // cookie settings for localhost
-  const cookieOptions = {
-    httpOnly: true,
-    secure: false,
-    sameSite: "lax",
-  };
-
-  //setting the access token
-  res.cookie("accessToken", accessToken, {
-    ...cookieOptions,
-    maxAge: 15 * 60 * 1000, // expires in 15 minutes
-  });
-
-  //setting the refresh token
-  res.cookie("refreshToken", refreshToken, {
-    ...cookieOptions,
-    maxAge: 7 * 24 * 60 * 60 * 1000, // expires in 7 days
-  });
-
-  console.log("Cookies set successfully");
-};
 
 export const createUser = async (req, res) => {
   const { email, password, username } = req.body;
---
+++ backend/utils/tokenUtils.js
@@ -0,0 +1,49 @@
+import jwt from "jsonwebtoken";
+import { redis } from "../lib/redis.js";
+
+// 15 minutes for storage to host the token
+export const generateTokens = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+  // 7 days for storage to host the token
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+  //returning the tokens
+  return { accessToken, refreshToken };
+};
+
+// 7 days for storage to host the token, function for storing the refresh token
+export const storageRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refreshToken:${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60
+  );
+};
+
+//* "accessToken" is the key name, and accessToken is the value, function for setting the cookies
+export const setCookies = (res, accessToken, refreshToken) => {
+  // cookie settings for localhost
+  const cookieOptions = {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+  };
+
+  //setting the access token
+  res.cookie("accessToken", accessToken, {
+    ...cookieOptions,
+    maxAge: 15 * 60 * 1000, // expires in 15 minutes
+  });
+
+  //setting the refresh token
+  res.cookie("refreshToken", refreshToken, {
+    ...cookieOptions,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // expires in 7 days
+  });
+
+  console.log("Cookies set successfully");
+};
